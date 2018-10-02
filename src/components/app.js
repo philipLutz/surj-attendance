a857e5909340db8d90cac986928e3f3f9ab94207
Philip Lutz <philiplutz413@gmail.com>
2018-10-01 17:22:15 -0700

    organized and started components

---
+++ src/components/app.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Route, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
+import Navbar from './navigation/navigation';
+import LoginPage from './pages/LoginPage';
+import AccountPage from './pages/AccountPage';
+import OnboardingPage from './pages/OnboardingPage';
+import SignupPage from './pages/SignupPage';
+import { refreshAuthToken, storeAuthInfo } from '../actions/auth';
+import './app.css';
+
+export class App extends React.Component {
+	componentDidMount() {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            storeAuthInfo(token, this.props.dispatch)
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (!prevProps.loggedIn && this.props.loggedIn) {
+            this.startPeriodicRefresh();
+        } else if (prevProps.loggedIn && !this.props.loggedIn) {
+            this.stopPeriodicRefresh();
+        }
+    }
+    componentWillUnmount() {
+        this.stopPeriodicRefresh();
+    }
+    startPeriodicRefresh() {
+        this.refreshInterval = setInterval(
+            () => this.props.dispatch(refreshAuthToken()),
+            60 * 60 * 1000
+        );
+    }
+    stopPeriodicRefresh() {
+        if (!this.refreshInterval) {
+            return;
+        }
+        clearInterval(this.refreshInterval);
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="app">
+                    <div id="nav-container">
+                        <Navbar />
+                    </div>
+                    <main className="content">
+                        <Route exact path="/" component={OnboardingPage} />
+                        <Route exact path="/home" component={OnboardingPage} />
+                        <Route exact path="/login" component={LoginPage} />
+                        <Route exact path="/signup" component={SignupPage} />
+                        <Route exact path="/account" component={AccountPage} />
+                    </main>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+	hasAuthToken: state.auth.authToken !== null,
+	loggedIn: state.auth.currentUser !== null
+});
+
+export default withRouter(connect(mapStateToProps)(App));
