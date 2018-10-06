2212dbc1f51b15060e121587a7504e532415b83c
Philip Lutz <philiplutz413@gmail.com>
2018-10-06 16:31:50 -0700

    onboarding page started

--- src/components/pages/OnboardingPage.js
+++ src/components/pages/OnboardingPage.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './OnboardingPage-style.css';
 
 export class OnboardingPage extends React.Component {
 	render() {
-		return (
-			<div className="onboarding-page">
-				<h1>SURJ Attendance tracker</h1>
+		let button = (
+			<div className="button-container">
+				<Link to="/login">
+					<button className="login-button">Login</button>
+				</Link>
 			</div>
 		)
+		return (
+			<section className="onboarding-page">
+				<h1>SURJ Attendance tracker</h1>
+				<p>If you don't know what this is for, you probably don't need to be here.<p>
+				<div className="button">{button}</div>
+			</section>
+		);
 	}
 }
 
